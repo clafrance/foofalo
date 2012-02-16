58371995673cea7fa6f67598a220723a386a47bc
Christie Lafrance <cl@CL.local>
2012-02-16 14:15:41 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foofalo::Application.config.secret_token = '0d73ac8ea6713bbeda0648088c5e9cca5140f9a6d3b98e966ce852747fede34ca66d14ec33ffa591146179c094aef846e5b85ad474dffb1e477e30fbf9921ab0'
