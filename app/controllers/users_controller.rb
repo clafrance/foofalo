bc83eebf507a81a0d9c2b49d4604c1d06e97679e
Christie Lafrance <cl@CL.local>
2012-02-17 16:37:58 -0800

    added requiring user sign in to update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, :only => [:edit, :update]
   
   def show
   end 
@@ -22,4 +23,20 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+    def signed_in_user
+      redirect_to signin_path, notice: "Please sign in." if current_user.nil?
+    end
 end
