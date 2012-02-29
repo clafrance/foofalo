92f4006e4299c3efb8c97a8ce0b924b8c99c513f
Christie Lafrance <christie.lafrance@gmail.com>
2012-02-28 22:16:19 -0800

    Deleted unwanted files

--- db/migrate/20120228001310_add_parent_name_to.rb
+++
@@ -1,9 +0,0 @@
-class AddParentNameTo < ActiveRecord::Migration
-  def up
-    add_column :users, :parent_firstname, :string
-    add_column :users, :parent_lastname, :string
-  end
-
-  def down
-  end
-end
