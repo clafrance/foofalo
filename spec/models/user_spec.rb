de9b7d040426874f7408e4c874007ce2266950bd
Christie Lafrance <christie.lafrance@gmail.com>
2012-03-11 23:26:22 -0700

    Authentication completed with tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,77 +2,78 @@ require 'spec_helper'
 
 describe User do
 
-  describe "#send_password_reset" do
-    let(:user) { Factory(:user) }
+  describe "test methods in user model" do
+    describe "#send_password_reset" do
+      let(:user) { Factory(:user) }
   
-    it "generates a unique password_reset_token each time" do
-      user.send_password_reset
-      last_token = user.password_reset_token
-      user.send_password_reset
-      user.password_reset_token.should_not eq(last_token)
+      it "generates a unique password_reset_token each time" do
+        user.send_password_reset
+        last_token = user.password_reset_token
+        user.send_password_reset
+        user.password_reset_token.should_not eq(last_token)
+      end
+  
+  
+      it "saves the time the password reset was sent" do
+        user.send_password_reset
+        user.reload.password_reset_sent_at.should be_present
+      end
+  
+      it "delivers email to user" do
+        user.send_password_reset
+        last_email.to.should include(user.email)
+      end
     end
   
+    describe "#send_username_reminder" do
+      let(:user) { Factory(:user) }
   
-    it "saves the time the password reset was sent" do
-      user.send_password_reset
-      user.reload.password_reset_sent_at.should be_present
+      it "delivers email to user" do
+        user.send_username_reminder
+        last_email.to.should include(user.email)
+      end
     end
-  
-    it "delivers email to user" do
-      user.send_password_reset
-      last_email.to.should include(user.email)
-    end
-  end
-  
-  describe "#send_username_reminder" do
-    let(:user) { Factory(:user) }
-  
-    it "delivers email to user" do
-      user.send_username_reminder
-      last_email.to.should include(user.email)
-    end
-  end
 
-  describe "#send_inform_parents" do
-    let(:user) { Factory(:user) }
+    describe "#send_inform_parents" do
+      let(:user) { Factory(:user) }
   
-    it "generates a unique inform_parents_token each time" do
-      user.send_inform_parents
-      last_token = user.inform_parents_token
-      user.send_inform_parents
-      user.inform_parents_token.should_not eq(last_token)
+      it "generates a unique inform_parents_token each time" do
+        user.send_inform_parents
+        last_token = user.inform_parents_token
+        user.send_inform_parents
+        user.inform_parents_token.should_not eq(last_token)
+      end
+  
+      it "saves the time the inform parents was sent" do
+        user.send_inform_parents
+        user.reload.inform_parents_sent_at.should be_present
+      end
+  
+      it "delivers email to user" do
+        user.send_inform_parents
+        last_email.to.should include(user.email)
+      end
     end
   
-    it "saves the time the inform parents was sent" do
-      user.send_inform_parents
-      user.reload.inform_parents_sent_at.should be_present
-    end
+    describe "#send_parent_confirm" do
+      let(:user) { Factory(:user) }
   
-    it "delivers email to user" do
-      user.send_inform_parents
-      last_email.to.should include(user.email)
+      it "sets the 'parent_approved' to Yes" do
+        user.send_parent_confirm
+        user.reload.parent_approved.should eq("Yes")
+      end
+  
+      it "sets the parent_approved_atto current time" do
+        user.send_parent_confirm
+        user.reload.parent_approved_at.should be_present
+      end
+  
+      it "delivers email to user" do
+        user.send_parent_confirm
+        last_email.to.should include(user.email)
+      end
     end
   end
-  
-  describe "#send_parent_confirm" do
-    let(:user) { Factory(:user) }
-  
-    it "sets the 'parent_approved' to Yes" do
-      user.send_parent_confirm
-      user.reload.parent_approved.should eq("Yes")
-    end
-  
-    it "sets the parent_approved_atto current time" do
-      user.send_parent_confirm
-      user.reload.parent_approved_at.should be_present
-    end
-  
-    it "delivers email to user" do
-      user.send_parent_confirm
-      last_email.to.should include(user.email)
-    end
-  end
-  
   
   describe "test user validations" do
     before do
