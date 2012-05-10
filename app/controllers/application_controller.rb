f443da385267c5d6c2fe8dfdf98b4f073298da0b
Christie Lafrance <christie.lafrance@gmail.com>
2012-05-10 14:03:35 -0700

    added display joke count and answer count for user index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   helper_method :current_user # make it available to views by passing it as a symble
   helper_method :full_name
   helper_method :random_challenge
+  helper_method :jokes_entered_by_user
+  helper_method :correct_answers_by_user
+  
   
   
   # helper is available to views by default, include make it available to controllers
